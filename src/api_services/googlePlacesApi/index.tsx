cd48d28092f51b8c2945240967a72c47987fc32b
sunkanmioguntimehin1-cyber <sunkanmioguntimehin1@gmail.com>
2026-02-02 18:44:36 -0800

    commit changes

--- src/api_services/googlePlacesApi/index.tsx
+++ src/api_services/googlePlacesApi/index.tsx
@@ -4,10 +4,8 @@ export const getSearchOption = async (value: any) => {
   try {
     const res = await axiosInstance.get(
       `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(
-        value
-      )}&components=country:us&types=geocode&key=${
-        process.env.EXPO_PUBLIC_GOOGLE_AUTOCOMPLETE_KEY
-      }`
+        value,
+      )}&components=country:us|country:ca&types=geocode&key=${process.env.EXPO_PUBLIC_GOOGLE_AUTOCOMPLETE_KEY}`,
     );
     const result = res.data;
     return result;
@@ -19,7 +17,7 @@ export const getSearchOption = async (value: any) => {
 export const fetchLocationDetails = async (placeId: string | undefined) => {
   try {
     const response = await fetch(
-      `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${process.env.EXPO_PUBLIC_GOOGLE_AUTOCOMPLETE_KEY}`
+      `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${process.env.EXPO_PUBLIC_GOOGLE_AUTOCOMPLETE_KEY}`,
     );
     const data = await response.json();
     const location = data.result.geometry.location;
@@ -28,25 +26,23 @@ export const fetchLocationDetails = async (placeId: string | undefined) => {
     const addressComponents = data.result.address_components;
     const state =
       addressComponents.find((component: any) =>
-        component.types.includes("administrative_area_level_1")
+        component.types.includes("administrative_area_level_1"),
       )?.long_name || "";
     const country =
       addressComponents.find((component: any) =>
-        component.types.includes("country")
+        component.types.includes("country"),
       )?.short_name || "";
     const zipCode =
       addressComponents.find((component: any) =>
-        component.types.includes("postal_code")
+        component.types.includes("postal_code"),
       )?.long_name || "";
     const city =
       addressComponents.find(
         (component: any) =>
           component.types.includes("locality") ||
-          component.types.includes("sublocality")
+          component.types.includes("sublocality"),
       )?.long_name || "";
     const address = data.result.formatted_address || "";
-
-  
 
     return {
       latitude: location.lat,
@@ -66,7 +62,7 @@ export const fetchLocationDetails = async (placeId: string | undefined) => {
 export const reserveGeo = async (lat: any, lng: any) => {
   try {
     const res = await axiosInstance.get(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.EXPO_PUBLIC_GOOGLE_AUTOCOMPLETE_KEY}`
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.EXPO_PUBLIC_GOOGLE_AUTOCOMPLETE_KEY}`,
     );
     const response = res.data;
     if (response.results && response.results.length > 0) {
@@ -76,7 +72,6 @@ export const reserveGeo = async (lat: any, lng: any) => {
     } else {
       console.error("No results found.");
     }
-   
   } catch (error) {
     throw error;
   }
