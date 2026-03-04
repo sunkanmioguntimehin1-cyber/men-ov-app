987413b18844deea79d8c981878c69fb9629b328
sunkanmioguntimehin1-cyber <sunkanmioguntimehin1@gmail.com>
2026-03-04 04:18:31 -0800

    commit changes

--- src/api_services/chatApi/chatMutation.ts
+++ src/api_services/chatApi/chatMutation.ts
@@ -1,23 +1,19 @@
-import { handleAxiosError } from "@/src/lib/handleAxiosError";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useRouter } from "expo-router";
-import { talkToChatAi } from ".";
 
-export const useTalkToChatAi = () => {
-  const router = useRouter();
-  const queryClient = useQueryClient();
-  return useMutation({
-    mutationFn: talkToChatAi,
-    async onSuccess(data: any) {
-      queryClient.invalidateQueries({
-        queryKey: ["get-chat-history"],
-      });
-    },
-    onError(error: any) {
-      handleAxiosError(error);
-    },
-  });
-};
+// export const useTalkToChatAi = () => {
+//   const router = useRouter();
+//   const queryClient = useQueryClient();
+//   return useMutation({
+//     mutationFn: talkToChatAi,
+//     async onSuccess(data: any) {
+//       queryClient.invalidateQueries({
+//         queryKey: ["get-chat-history"],
+//       });
+//     },
+//     onError(error: any) {
+//       handleAxiosError(error);
+//     },
+//   });
+// };
 
 // export const useTalkToChatAiwithStream = () => {
 //   const router = useRouter();
